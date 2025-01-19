1e1dc2dce333d9a483eb1fd27f4b65267ed697f2
rendyelang <lsmnrndy123@gmail.com>
2025-01-19 15:21:43 +0700

    added product model for query to database

--- src/models/products.js
+++ src/models/products.js
@@ -1,18 +1,50 @@
 const db = require("../config/db")
 
-// GET all products
+// GET: read all products
 const getAllProducts = () => {
     const sqlQuery = "SELECT * FROM products"
-    return db.promise().query(sqlQuery)
+    const action =  db.promise().query(sqlQuery)
+    console.log(action)
+    return action
 }
 
-// GET product by ID
+// GET: read product by ID
 const getProductById = (id) => {
     const sqlQuery = "SELECT * FROM products WHERE product_id = ?"
     return db.promise().query(sqlQuery, [id])
 }
 
+//  GET: raad product by category
+const getProductByCategory = (category) => {
+    const sqlQuery = "SELECT * FROM products WHERE category = ?"
+    return db.promise().query(sqlQuery, [category])
+}
+
+// POST: create a new product
+const createProduct = (newProduct) => {
+    const {category, name, price, stock_quantity} = newProduct
+    const sqlQueery = "INSERT INTO products (category, name, price, stock_quantity) VALUES (?, ?, ?, ?)"
+    return db.promise().query(sqlQueery, [category, name, price, stock_quantity])
+}
+
+// PUT: edit a product by ID
+const editProduct = (id, editedProduct) => {
+    const {category, name, price, stock_quantity} = editedProduct
+    const sqlQuery = "UPDATE products SET category = ?, name = ?, price = ?, stock_quantity = ? WHERE product_id = ?"
+    return db.promise().query(sqlQuery, [category, name, price, stock_quantity, id])
+}
+
+// DELETE: delete product by ID
+const deleteProduct = (id) => {
+    const sqlQuery = "DELETE FROM products WHERE product_id = ?"
+    return db.promise().query(sqlQuery, [id])
+}
+
 module.exports = {
     getAllProducts,
-    getProductById
+    getProductById,
+    getProductByCategory,
+    createProduct,
+    editProduct,
+    deleteProduct
 }
