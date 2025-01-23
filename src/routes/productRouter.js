2c0c78273350ee4cea2a36392f5c758ab3469efd
rendyelang <lsmnrndy123@gmail.com>
2025-01-23 20:22:19 +0700

    added auth and admin middleware

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -7,6 +7,10 @@ const {
     editProduct,
     deleteProduct
     } = require("../controllers/productController")
+
+const {isAuthToken} = require("../middleware/employeeAuthProtect")
+const isAdmin = require("../middleware/isAdmin")
+
 const router = express.Router()
 
 // GET - Read all products data
@@ -19,12 +23,12 @@ router.get("/product/search", getProductByCategory)
 router.get("/product/:id", getProductById)
 
 // POST - Create a new product
-router.post("/add-product", createProduct)
+router.post("/add-product", isAuthToken, isAdmin, createProduct)
 
 // PUT - Edit a product
-router.put("/product/edit/:id", editProduct)
+router.put("/product/edit/:id", isAuthToken, isAdmin, editProduct)
 
 // DELETE = Delete a product by ID
-router.delete("/product/delete/:id", deleteProduct)
+router.delete("/product/delete/:id", isAuthToken, isAdmin, deleteProduct)
 
 module.exports = router
